983de9b0a43279c72e7ad843bb29228b4ac5a0cb
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2014-06-10 15:20:10 +0200

    revised haml generator specs

--- spec/generators/haml/command_controller/command_controller_generator_spec.rb
+++ spec/generators/haml/command_controller/command_controller_generator_spec.rb
@@ -2,33 +2,47 @@ require 'spec_helper'
 require 'generators/haml/command_controller/command_controller_generator'
 
 describe Haml::Generators::CommandControllerGenerator, :type => :generator do
-  it "creates the view with defaults" do
-    gen = generator %w(create_something)
-    expect(gen).to receive(:create_template_file)
-    capture(:stdout) { gen.invoke_all }
-  end
-
-  it "creates the view with specified action name" do
-    gen = generator %w(create_something create)
-    expect(gen).to receive(:create_template_file)
-    capture(:stdout) { gen.invoke_all }
-  end
-
-  it "creates the view with specified action name and attributes" do
-    gen = generator %w(create_something create name:string:required active:boolean other)
+  it "should run all tasks in the generator" do
+    gen = generator %w(foo_bar)
     expect(gen).to receive(:create_template_file)
     capture(:stdout) { gen.invoke_all }
   end
 
   describe "the generated files" do
-    before do
-      run_generator %w(create_something create name:string:required active:boolean other)
-    end
-
     describe "the view" do
-      subject { file('app/views/create_something_command/create.html.haml') }
+      describe "with defaults" do
+        before do
+          run_generator %w(foo_bar)
+        end
 
-      it { should exist }
+        subject { file('app/views/foo_bar_command/new.html.haml') }
+
+        it { should exist }
+        it { should contain('= command_form_for') }
+      end
+
+      describe "with specified action name" do
+        before do
+          run_generator %w(foo_bar create)
+        end
+
+        subject { file('app/views/foo_bar_command/create.html.haml') }
+
+        it { should exist }
+        it { should contain('= command_form_for') }
+      end
+
+      describe "with specified action name and attributes" do
+        before do
+          run_generator %w(foo_bar create name:string:required active:boolean other)
+        end
+
+        subject { file('app/views/foo_bar_command/create.html.haml') }
+
+        it { should exist }
+        it { should contain('= command_form_for') }
+        it { should contain('= f.input :name') }
+      end
     end
   end
 end
--- spec/generators/haml/enquiry_controller/enquiry_controller_generator_spec.rb
+++ spec/generators/haml/enquiry_controller/enquiry_controller_generator_spec.rb
@@ -2,33 +2,47 @@ require 'spec_helper'
 require 'generators/haml/enquiry_controller/enquiry_controller_generator'
 
 describe Haml::Generators::EnquiryControllerGenerator, :type => :generator do
-  it "creates the view with defaults" do
-    gen = generator %w(list_something)
-    expect(gen).to receive(:create_template_file)
-    capture(:stdout) { gen.invoke_all }
-  end
-
-  it "creates the view with specified action name" do
-    gen = generator %w(list_something list)
-    expect(gen).to receive(:create_template_file)
-    capture(:stdout) { gen.invoke_all }
-  end
-
-  it "creates the view with specified action name and attributes" do
-    gen = generator %w(list_something list name:string:required active:boolean other)
+  it "should run all tasks in the generator" do
+    gen = generator %w(foo_bar)
     expect(gen).to receive(:create_template_file)
     capture(:stdout) { gen.invoke_all }
   end
 
   describe "the generated files" do
-    before do
-      run_generator %w(list_something list name:string:required active:boolean other)
-    end
-
     describe "the view" do
-      subject { file('app/views/list_something_enquiry/list.html.haml') }
+      describe "with defaults" do
+        before do
+          run_generator %w(foo_bar)
+        end
 
-      it { should exist }
+        subject { file('app/views/foo_bar_enquiry/index.html.haml') }
+
+        it { should exist }
+        it { should contain('= enquiry_form_for') }
+      end
+
+      describe "with specified action name" do
+        before do
+          run_generator %w(foo_bar list)
+        end
+
+        subject { file('app/views/foo_bar_enquiry/list.html.haml') }
+
+        it { should exist }
+        it { should contain('= enquiry_form_for') }
+      end
+
+      describe "with specified action name and attributes" do
+        before do
+          run_generator %w(foo_bar list name:string:required active:boolean other)
+        end
+
+        subject { file('app/views/foo_bar_enquiry/list.html.haml') }
+
+        it { should exist }
+        it { should contain('= enquiry_form_for') }
+        it { should contain('= f.input :name') }
+      end
     end
   end
 end

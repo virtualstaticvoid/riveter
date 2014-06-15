218dd9ccb1bad0526b6684217f8f0954e478f2cd
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2014-06-15 19:29:58 +0200

    updated coverage configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,18 @@
 require 'bundler/setup'
 Bundler.setup
 
+require 'simplecov'
 require 'coveralls'
 
-Coveralls.wear!
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter 'spec'
+  add_filter 'vendor'
+end
 
 require 'pry'
 

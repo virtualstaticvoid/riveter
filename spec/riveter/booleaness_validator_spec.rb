3c297e80b2f0d2180843b31ec2110e7880366fb7
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2014-06-22 18:15:49 +0200

    updated booleaness validator spec

--- spec/riveter/booleaness_validator_spec.rb
+++ spec/riveter/booleaness_validator_spec.rb
@@ -16,68 +16,66 @@ describe Riveter::BooleanessValidator do
       errors
     end
 
-    context "valid data" do
-      describe "with defaults" do
-        it {
-          validator = subject.new(:attributes => [:test])
-          record = record_for('not_a_boolean')
-          expect(record).to receive(:errors).and_return(errors)
+    describe "with defaults" do
+      it {
+        validator = subject.new(:attributes => [:test])
+        record = record_for('not_a_boolean')
+        expect(record).to receive(:errors).and_return(errors)
 
-          validator.validate(record)
-        }
+        validator.validate(record)
+      }
 
-        it {
-          validator = subject.new(:attributes => [:test])
-          record = record_for(true)
-          expect(record).to_not receive(:errors)
+      it {
+        validator = subject.new(:attributes => [:test])
+        record = record_for(true)
+        expect(record).to_not receive(:errors)
 
-          validator.validate(record)
-        }
+        validator.validate(record)
+      }
 
-        it {
-          validator = subject.new(:attributes => [:test])
-          record = record_for(false)
-          expect(record).to_not receive(:errors)
+      it {
+        validator = subject.new(:attributes => [:test])
+        record = record_for(false)
+        expect(record).to_not receive(:errors)
 
-          validator.validate(record)
-        }
-      end
+        validator.validate(record)
+      }
+    end
 
-      describe "with allow_nil" do
-        it {
-          validator = subject.new(:attributes => [:test], :allow_nil => false)
-          record = record_for(nil)
-          expect(record).to receive(:errors).and_return(errors)
+    describe "with allow_nil" do
+      it {
+        validator = subject.new(:attributes => [:test], :allow_nil => false)
+        record = record_for(nil)
+        expect(record).to receive(:errors).and_return(errors)
 
-          validator.validate(record)
-        }
+        validator.validate(record)
+      }
 
-        it {
-          validator = subject.new(:attributes => [:test], :allow_nil => true)
-          record = record_for(nil)
-          expect(record).to_not receive(:errors)
+      it {
+        validator = subject.new(:attributes => [:test], :allow_nil => true)
+        record = record_for(nil)
+        expect(record).to_not receive(:errors)
 
-          validator.validate(record)
-        }
-      end
+        validator.validate(record)
+      }
+    end
 
-      describe "with allow_blank" do
-        it {
-          validator = subject.new(:attributes => [:test], :allow_blank => false)
-          record = record_for('')
-          expect(record).to receive(:errors).and_return(errors)
+    describe "with allow_blank" do
+      it {
+        validator = subject.new(:attributes => [:test], :allow_blank => false)
+        record = record_for('')
+        expect(record).to receive(:errors).and_return(errors)
 
-          validator.validate(record)
-        }
+        validator.validate(record)
+      }
 
-        it {
-          validator = subject.new(:attributes => [:test], :allow_blank => true)
-          record = record_for('')
-          expect(record).to_not receive(:errors)
+      it {
+        validator = subject.new(:attributes => [:test], :allow_blank => true)
+        record = record_for('')
+        expect(record).to_not receive(:errors)
 
-          validator.validate(record)
-        }
-      end
+        validator.validate(record)
+      }
     end
   end
 end

c26114e393e0e0c9ce9ec699a7f17821a666f986
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-03-07 14:43:41 +0200

    improvements to query filter template

--- lib/generators/riveter/query_filter/templates/query_filter.rb
+++ lib/generators/riveter/query_filter/templates/query_filter.rb
@@ -11,30 +11,34 @@ class <%= class_name %>QueryFilter < Riveter::QueryFilter::Base
   #
   # E.g.
   #
-  #   attr_string :name[, options]
+  #   attr_string :name[, options] [do |value|
+  #     # optional converter logic
+  #     value.to_i
+  #   end]
   #
   #   options include:
-  #    :required => true|false  # default is false
-  #    :default => 'a value'    # default is nil
-  #    :validate => true|false  # default is true
+  #    :required => true|false            # default is false
+  #    :default => 'a value' or lambda    # default is nil
+  #    :validate => true|false            # default is true
+  #
+  #   optionally, provide a block which takes one argument and returns the converted value
   #
   #  supported attributes:
   #
-  #   attr_string
-  #   attr_text
-  #   attr_integer
-  #   attr_decimal
-  #   attr_date
-  #   attr_date_range
-  #   attr_time
-  #   attr_boolean
-  #   attr_enum
-  #   attr_array
-  #   attr_hash
-  #   attr_model
+  #   attr_string      :attr_name
+  #   attr_text        :attr_name
+  #   attr_integer     :attr_name
+  #   attr_decimal     :attr_name
+  #   attr_date        :attr_name
+  #   attr_date_range  :attr_name[, :min => <date>, :max => <date>]
+  #   attr_time        :attr_name
+  #   attr_boolean     :attr_name
+  #   attr_enum        :attr_name, <enum_module>
+  #   attr_array       :attr_name[, :data_type => :integer]
+  #   attr_hash        :attr_name[, :data_type => :integer]
   #
-
-  # optionally, define additional validations
+  # optionally, define additional validations and helper methods
+  #
 <% end -%>
 
 end
